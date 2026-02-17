410e1f0b12ab0609ffc97c8803ca73c61311e312
quangvu0407 <trantuyenquang0407@gmail.com>
2026-02-17 14:40:08 +0700

    config server mongodb cloudinary

--- server.js
+++ server.js
@@ -1,11 +1,16 @@
 import express from "express"
 import cors from "cors"
 import 'dotenv/config'
+import connectDB from "./config/mongodb.js";
+import connectCloudinary from "./config/cloudinary.js";
 
 //App config
 
 const app = express();
 const port = process.env.PORT || 4000
+
+connectDB()
+connectCloudinary()
 
 //middleware
 app.use(express.json())
